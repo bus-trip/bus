5adde95e48c86b45d9e4203db58afb2f5813489f
Andrew <tsm.andrew@gmail.com>
2014-10-02 22:01:18 +0400

    core

---
+++ protected/views/trips/sheet.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Андрей
+ * Date: 01.10.14
+ * Time: 17:48
+ */
+?>
+
+<h2>Посадочная ведомость</h2>
+Направление: <?php echo $dataHeader['direction']['startPoint'] .' - '. $dataHeader['direction']['endPoint']; ?><br/>
+Отправление: <?php echo $dataHeader['trips']['departure']; ?>&nbsp;&nbsp;&nbsp;&nbsp;Прибытие: <?php echo $dataHeader['trips']['arrival']; ?><br/>
+Автобус: <?php echo $dataHeader['bus']['model'] .', номер '. $dataHeader['bus']['number']; ?><br/>
+
+<?php
+
+$this->widget('zii.widgets.grid.CGridView', array(
+    'id'=>'trips-sheet',
+    'dataProvider'=>$dataProvider,
+    'template'=>'{items}',
+    'columns'=>array(
+        array(
+            'name'=>'place',
+            'header'=>'Место',
+        ),
+        array(
+            'name'=>'passenger',
+            'header'=>'ФИО',
+        ),
+        array(
+            'name'=>'startPoint',
+            'header'=>'Посадка',
+        ),
+        array(
+            'name'=>'endPoint',
+            'header'=>'Высадка',
+        ),
+        array(
+            'name'=>'phone',
+            'header'=>'Номер телефона',
+        ),
+        array(
+            'name' => 'price',
+            'header' => 'Стоимость',
+        ),
+    ),
+));
+
+echo '<a href="'. Yii::app()->createUrl('trips/sheetprint',array('id'=>$_GET['id'])) .'" target="_blank">Версия для печати</a>';
+
+?>
