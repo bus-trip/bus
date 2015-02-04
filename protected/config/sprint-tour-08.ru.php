3031c24b5e9624c82d96f3b8846b6c76053581df
User Sprinttour <sprinttour@webjails.ru>
2015-02-04 13:17:07 +0300

    commit on production :)

---
+++ protected/config/sprint-tour-08.ru.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+	'components' => array(
+		'db' => array(
+			'connectionString' => 'mysql:host=localhost;dbname=sprinttour',
+			'emulatePrepare'   => TRUE,
+			'username'         => 'sprinttourroot',
+			'password'         => 'bAtOyutk8E1B',
+			'charset'          => 'utf8',
+		),
+	),
+);
