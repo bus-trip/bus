9669db757da1a948961737ee63e80b15ee46eb6f
sumarokov <a.sumarokov@gmail.com>
2015-11-09 23:35:12 +0300

    yiic unreserve tickets

---
+++ protected/commands/commonCommand.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Part of bus 2015
+ * Created by: Alexander Sumarokov on 09.11.2015:22:52
+ */
+class commonCommand extends CConsoleCommand
+{
+	public function actionTest()
+	{
+		Yii::import('application.models.TempReserve');
+
+		$criteria            = new CDbCriteria();
+		$criteria->condition = 'CONVERT(created, SIGNED INTEGER) < :created';
+		$criteria->params    = array(
+			':created' => time() + 15 * 60,
+		);
+		TempReserve::model()->deleteAll($criteria);
+	}
+}
