ae82f40074930726ba492c414ad989dedf0ebff6
sumarokov <a.sumarokov@gmail.com>
2016-03-13 18:15:24 +0300

    admin theme

--- themes/bus/views/layouts/main.php
+++ themes/bus/views/layouts/main.php
@@ -46,6 +46,18 @@ $clientScript
 				<li><a href="#" class="link link_c-white">Акции</a></li>
 				<li><a href="#" class="link link_c-white">Контакты</a></li>
 			</ul>
+			<?php if (Yii::app()->user->name == 'admin') { ?>
+				<?php $this->widget('zii.widgets.CMenu', [
+					'items' => [
+						['label' => 'Рейсы на месяц', 'url' => ['/admin/index']],
+						['label' => 'Автобусы', 'url' => ['/buses/admin']],
+						['label' => 'Рейсы', 'url' => ['/trips/admin/status/actual']],
+						['label' => 'Направления', 'url' => ['/directions/admin']],
+						['label' => 'Все пассажиры', 'url' => ['/tickets/passengers?Profiles_sort=last_name']],
+						['label' => 'Оформление билета', 'url' => ['/UserInterface/default/index']],
+					],
+				]); ?>
+			<?php } ?>
 		</div>
 		<div class="header__item">
 			<ul class="login-block">
@@ -62,20 +74,7 @@ $clientScript
 		</div>
 	</div>
 </div>
-<?php if (Yii::app()->user->name == 'admin') { ?>
-	<div id="mainmenu">
-		$this->widget('zii.widgets.CMenu', [
-		'items' => [
-		['label' => 'Рейсы на месяц', 'url' => ['/admin/index']],
-		['label' => 'Автобусы', 'url' => ['/buses/admin']],
-		['label' => 'Рейсы', 'url' => ['/trips/admin/status/actual']],
-		['label' => 'Направления', 'url' => ['/directions/admin']],
-		['label' => 'Все пассажиры', 'url' => ['/tickets/passengers?Profiles_sort=last_name']],
-		['label' => 'Оформление билета', 'url' => ['/UserInterface/default/index']],
-		],
-		]);
-	</div>
-<?php } ?>
+
 <div class="content-form">
 	<div class="content-form__layout">
 		<!-- mainmenu -->
