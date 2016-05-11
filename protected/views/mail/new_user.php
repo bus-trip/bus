bb9cbca6243959e4fedf9ad64012b00f04a153d5
sumarokov <a.sumarokov@gmail.com>
2016-05-12 00:29:51 +0300

    email on new user

---
+++ protected/views/mail/new_user.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Part of bus 2016
+ * Created by: Alexander Sumarokov on 12.05.2016:0:28
+ */
+?>Здравствуйте!<br><br>
+Вы зарегистрированы на портале <?= CHtml::encode(Yii::app()->name) ?> с логином <?= $user ?>.<br>
