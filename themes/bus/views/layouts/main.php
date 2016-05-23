22b43b1f0e1d0f58fbe7d64a16f5f0f53a3d388b
sumarokov <a.sumarokov@gmail.com>
2016-05-24 02:27:15 +0300

    Главная страница

--- themes/bus/views/layouts/main.php
+++ themes/bus/views/layouts/main.php
@@ -10,6 +10,7 @@ $clientScript
 	->registerCssFile($baseUrl . '/css/form.css')
 	->registerCssFile($baseUrl . '/themes/bus/css/main.css')
 	->registerScriptFile($baseUrl . '/themes/bus/js/plugins.js')
+	->registerScriptFile($baseUrl . '/themes/bus/js/singlepagenav.jquery.js')
 	->registerScriptFile($baseUrl . '/themes/bus/js/main.js');
 ?>
 
@@ -37,15 +38,27 @@ $clientScript
 <div class="header">
 	<div class="header__layout">
 		<div class="header__item">
-			<div class="logo">
-				<a href="/"
-				   title="<?php echo CHtml::encode(Yii::app()->name); ?>"><?php echo CHtml::encode(Yii::app()->name); ?></a>
-			</div>
-			<ul class="nav-main">
-				<li><a href="#" class="link link_c-white link_active">О компании</a></li>
-				<li><a href="#" class="link link_c-white">Акции</a></li>
-				<li><a href="#" class="link link_c-white">Контакты</a></li>
-			</ul>
+			<?php if ($this->isFront()) { ?>
+				<div class="logo">
+					<a href="#top"
+					   title="<?php echo CHtml::encode(Yii::app()->name); ?>"><?php echo CHtml::encode(Yii::app()->name); ?></a>
+				</div>
+				<ul class="nav-main front">
+					<li><a href="#about" class="link link_c-white">О компании</a></li>
+					<li><a href="#actions" class="link link_c-white">Акции</a></li>
+					<li><a href="#contacts" class="link link_c-white">Контакты</a></li>
+				</ul>
+			<?php } else { ?>
+				<div class="logo">
+					<a href="<?= Yii::app()->createUrl('UserInterface/default/index/step/find')?>"
+					   title="<?php echo CHtml::encode(Yii::app()->name); ?>"><?php echo CHtml::encode(Yii::app()->name); ?></a>
+				</div>
+				<ul class="nav-main">
+					<li><a href="<?= Yii::app()->createUrl('UserInterface/default/index/step/find')?>#about" class="link link_c-white">О компании</a></li>
+					<li><a href="<?= Yii::app()->createUrl('UserInterface/default/index/step/find')?>#actions" class="link link_c-white">Акции</a></li>
+					<li><a href="<?= Yii::app()->createUrl('UserInterface/default/index/step/find')?>#contacts" class="link link_c-white">Контакты</a></li>
+				</ul>
+			<?php } ?>
 			<?php if (Yii::app()->user->name == 'admin') { ?>
 				<?php $this->widget('zii.widgets.CMenu', [
 					'items' => [
@@ -75,7 +88,7 @@ $clientScript
 	</div>
 </div>
 
-<div class="content-form">
+<div class="content-form" id="top">
 	<div class="content-form__layout">
 		<?php echo $content; ?>
 	</div>
@@ -85,7 +98,88 @@ $clientScript
 if ($this->isFront()) { ?>
 	<div class="content">
 		<div class="content__layout">
-			<h2 class="title title_ta-c title_fz-m">О компании</h2>
+			<h2 class="title title_ta-c title_fz-l" id="about">О компании</h2>
+			<div class="content__text">
+				<div class="content__image">
+					<img src="<?php echo Yii::app()->theme->getBaseUrl(); ?>/img/img_bus.jpg" alt="Автобус">
+				</div>
+				<div class="content__desc">
+					<h3 class="title title_ta-l title_fz-s">
+						Ежедневные поздки Элиста - Москва - Элиста
+					</h3>
+					<p class="text">
+						Коневодство ежегодно. Оазисное земледелие входит бесплатный расовый состав. Для пользования
+						телефоном-автоматом необходимы разменные монеты, однако субэкваториальный климат непосредственно
+						декларирует бальнеоклиматический курорт. Отгонное животноводство, несмотря на внешние
+						воздействия, точно применяет кандым.
+					</p>
+					<ul class="text">
+						<li>отправление из Элисты в 12:00</li>
+						<li>отправление из Москвы в 12:00</li>
+					</ul>
+				</div>
+			</div>
+
+			<div class="advantage">
+				<div class="advantage__item">
+					<div class="advantage__image"></div>
+					<p class="advantage__title">wi-fi</p>
+				</div>
+				<div class="advantage__item">
+					<div class="advantage__image"></div>
+					<p class="advantage__title">подушки и пледы</p>
+				</div>
+				<div class="advantage__item">
+					<div class="advantage__image"></div>
+					<p class="advantage__title">Телевизор</p>
+				</div>
+				<div class="advantage__item">
+					<div class="advantage__image"></div>
+					<p class="advantage__title">Вода</p>
+				</div>
+				<div class="advantage__item">
+					<div class="advantage__image"></div>
+					<p class="advantage__title">Бесплатное такси <br>в пределах МКАД</p>
+				</div>
+			</div>
+
+			<div class="content__text">
+				<div class="content__desc">
+					<p class="text">
+						Альбатрос прекрасно применяет вечнозеленый кустарник, а из холодных закусок можно выбрать
+						плоские колбасы "луканка" и "суджук". Флора и фауна перевозит вулканизм.
+					</p>
+				</div>
+			</div>
+
+			<h2 class="title title_ta-c title_fz-l" id="actions">Акции</h2>
+
+			<div class="content__text">
+				<div class="content__desc">
+					<p class="text">
+						Из первых блюд распространены супы-пюре и бульоны, но подают их редко, тем не менее производство
+						зерна и зернобобовых недоступно превышает символический центр современного Лондона, ни для кого
+						не секрет, что Болгария славится масличными розами, которые цветут по всей Казанлыкской долине.
+					</p>
+					<a href="#" class="btn btn_bg-pink">Подробнее</a>
+				</div>
+				<div class="content__image">
+					<img src="<?php echo Yii::app()->theme->getBaseUrl(); ?>/img/img_bus-1.jpg" alt="Автобус">
+				</div>
+			</div>
+			<h2 class="title title_ta-c title_fz-l" id="contacts">Контакты</h2>
+
+			<div class="content__text">
+				<div class="content__image">
+					<script type="text/javascript" charset="utf-8" async
+							src="https://api-maps.yandex.ru/services/constructor/1.0/js/?sid=cv_Cgnxo2kS8QKBMoGerEbcBT0OHIX5O&width=448&height=277&lang=ru_RU&sourceType=constructor&scroll=true"></script>
+				</div>
+				<div class="content__desc">
+					<p class="text">
+						БРОНИРОВАНИЕ ПО НОМЕРУ +7-905-400-0468
+					</p>
+				</div>
+			</div>
 		</div>
 	</div>
 <?php } ?>
