bced84d4f1ade95af0aaae86ea2a66d63873c1dc
tsmandrew <tsm.andrew@gmail.com>
2016-07-24 20:41:33 +0300

    Выпадалка только по направлениям в поиске билетов

--- themes/bus/views/layouts/main.php
+++ themes/bus/views/layouts/main.php
@@ -1,25 +1,23 @@
 <?php
-/*
- * @var $this Controller
- */
-
-$baseUrl      = Yii::app()->request->baseUrl;
+$baseUrl = Yii::app()->request->baseUrl;
 $clientScript = Yii::app()->getClientScript();
 $clientScript
 	->registerCssFile($baseUrl . '/css/main.css')
 	->registerCssFile($baseUrl . '/css/form.css')
-	->registerCssFile($baseUrl . '/themes/bus/css/main.css')
+	->registerCssFile($baseUrl . '/themes/bus/css/style.css')
 	->registerScriptFile($baseUrl . '/themes/bus/js/plugins.js')
 	->registerScriptFile($baseUrl . '/themes/bus/js/singlepagenav.jquery.js')
-	->registerScriptFile($baseUrl . '/themes/bus/js/main.js');
+	->registerScriptFile($baseUrl . '/themes/bus/js/main.js')
+	->registerScriptFile($baseUrl . '/themes/bus/js/fonts.js')
+	->registerScriptFile($baseUrl . '/themes/bus/js/dynamic.js');
 ?>
 
 <!DOCTYPE html>
-<html lang="ru">
-
+<html>
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width">
+	<title>Спринт-тур</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css"
 		  media="screen, projection"/>
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css"
@@ -32,32 +30,89 @@ $clientScript
 	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,700,400italic&subset=latin,cyrillic'
 		  rel='stylesheet' type='text/css'>
 </head>
-
 <body>
-
-<div class="header">
-	<div class="header__layout">
-		<div class="header__item">
+<div class="page">
+	<div class="header">
+		<div class="header__wrapper">
 			<?php if ($this->isFront()) { ?>
-				<div class="logo">
+				<div class="header__logo">
 					<a href="#top"
 					   title="<?php echo CHtml::encode(Yii::app()->name); ?>"><?php echo CHtml::encode(Yii::app()->name); ?></a>
 				</div>
-				<ul class="nav-main front">
-					<li><a href="#about" class="link link_c-white">О компании</a></li>
-					<li><a href="#actions" class="link link_c-white">Акции</a></li>
-					<li><a href="#contacts" class="link link_c-white">Контакты</a></li>
-				</ul>
+				<div class="header__container">
+					<div class="header__menu">
+						<div class="menu">
+							<a class="menu__item menu__item_active" href="#"><span
+									class="menu__name">О компании</span></a>
+							<a class="menu__item" href="#"><span class="menu__name">Расписание</span></a>
+							<a class="menu__item" href="#"><span class="menu__name">Контакты</span></a>
+							<a class="menu__item" href="#"><span class="menu__name">Акции</span></a>
+						</div>
+					</div>
+					<div class="header__authorization">
+						<div class="authorization">
+							<?php if (Yii::app()->user->isGuest) { ?>
+								<div class="authorization__reg">
+									<a href="<?php echo $this->createUrl('/user/register') ?>" rel="nofollow">Регистрация</a>
+								</div>
+								<div class="authorization__login">
+									<a href="<?php echo $this->createUrl('/user/login') ?>" rel="nofollow">Вход</a>
+								</div>
+							<?php } else { ?>
+								<div class="authorization__reg">
+									<a href="<?php echo $this->createUrl('/account'); ?>" rel="nofollow">Личный
+										кабинет</a>&nbsp;(<?php echo Yii::app()->user->name; ?>)
+								</div>
+								<div class="authorization__login">
+									<a href="<?php echo $this->createUrl('/user/logout') ?>" rel="nofollow">Выход</a>
+								</div>
+							<?php } ?>
+						</div>
+					</div>
+				</div>
 			<?php } else { ?>
-				<div class="logo">
-					<a href="<?= Yii::app()->createUrl('UserInterface/default/index/step/find')?>"
+				<div class="header__logo">
+					<a href="<?= Yii::app()->createUrl('UserInterface/default/index/step/find') ?>"
 					   title="<?php echo CHtml::encode(Yii::app()->name); ?>"><?php echo CHtml::encode(Yii::app()->name); ?></a>
 				</div>
-				<ul class="nav-main">
-					<li><a href="<?= Yii::app()->createUrl('UserInterface/default/index/step/find')?>#about" class="link link_c-white">О компании</a></li>
-					<li><a href="<?= Yii::app()->createUrl('UserInterface/default/index/step/find')?>#actions" class="link link_c-white">Акции</a></li>
-					<li><a href="<?= Yii::app()->createUrl('UserInterface/default/index/step/find')?>#contacts" class="link link_c-white">Контакты</a></li>
-				</ul>
+				<div class="header__container">
+					<div class="header__menu">
+						<div class="menu">
+							<a class="menu__item menu__item_active"
+							   href="<?= Yii::app()->createUrl('UserInterface/default/index/step/find') ?>#"><span
+									class="menu__name">О компании</span></a>
+							<a class="menu__item"
+							   href="<?= Yii::app()->createUrl('UserInterface/default/index/step/find') ?>#"><span
+									class="menu__name">Расписание</span></a>
+							<a class="menu__item"
+							   href="<?= Yii::app()->createUrl('UserInterface/default/index/step/find') ?>#"><span
+									class="menu__name">Контакты</span></a>
+							<a class="menu__item"
+							   href="<?= Yii::app()->createUrl('UserInterface/default/index/step/find') ?>#"><span
+									class="menu__name">Акции</span></a>
+						</div>
+					</div>
+					<div class="header__authorization">
+						<div class="authorization">
+							<?php if (Yii::app()->user->isGuest) { ?>
+								<div class="authorization__reg">
+									<a href="<?php echo $this->createUrl('/user/register') ?>" rel="nofollow">Регистрация</a>
+								</div>
+								<div class="authorization__login">
+									<a href="<?php echo $this->createUrl('/user/login') ?>" rel="nofollow">Вход</a>
+								</div>
+							<?php } else { ?>
+								<div class="authorization__reg">
+									<a href="<?php echo $this->createUrl('/account'); ?>" rel="nofollow">Личный
+										кабинет</a>&nbsp;(<?php echo Yii::app()->user->name; ?>)
+								</div>
+								<div class="authorization__login">
+									<a href="<?php echo $this->createUrl('/user/logout') ?>" rel="nofollow">Выход</a>
+								</div>
+							<?php } ?>
+						</div>
+					</div>
+				</div>
 			<?php } ?>
 			<?php if (Yii::app()->user->name == 'admin') { ?>
 				<?php $this->widget('zii.widgets.CMenu', [
@@ -72,116 +127,100 @@ $clientScript
 				]); ?>
 			<?php } ?>
 		</div>
-		<div class="header__item">
-			<ul class="login-block">
-				<?php if (Yii::app()->user->isGuest) { ?>
-					<li><a href="<?php echo $this->createUrl('/user/register') ?>" rel="nofollow">Регистрация</a></li>
-					<li><a href="<?php echo $this->createUrl('/user/login') ?>" rel="nofollow">Вход</a></li>
-				<?php } else { ?>
-					<li><a href="<?php echo $this->createUrl('/account'); ?>" rel="nofollow">Личный
-							кабинет</a>&nbsp;(<?php echo Yii::app()->user->name; ?>)
-					</li>
-					<li><a href="<?php echo $this->createUrl('/user/logout') ?>" rel="nofollow">Выход</a></li>
-				<?php } ?>
-			</ul>
-		</div>
 	</div>
-</div>
-
-<div class="content-form" id="top">
-	<div class="content-form__layout">
-		<?php echo $content; ?>
-	</div>
-</div>
-<?php
-
-if ($this->isFront()) { ?>
 	<div class="content">
-		<div class="content__layout">
-			<h2 class="title title_ta-c title_fz-l" id="about">О компании</h2>
-			<div class="content__text">
-				<div class="content__image">
-					<img src="<?php echo Yii::app()->theme->getBaseUrl(); ?>/img/img_bus.jpg" alt="Автобус">
-				</div>
-				<div class="content__desc">
-					<h3 class="title title_ta-l title_fz-s">
-						Ежедневные поздки Элиста - Москва - Элиста
-					</h3>
-					<p class="text">
-						Коневодство ежегодно. Оазисное земледелие входит бесплатный расовый состав. Для пользования
-						телефоном-автоматом необходимы разменные монеты, однако субэкваториальный климат непосредственно
-						декларирует бальнеоклиматический курорт. Отгонное животноводство, несмотря на внешние
-						воздействия, точно применяет кандым.
-					</p>
-					<ul class="text">
-						<li>отправление из Элисты в 12:00</li>
-						<li>отправление из Москвы в 12:00</li>
-					</ul>
-				</div>
+		<div class="main">
+			<?php echo $content; ?>
+
+
+			<!--			<div class="search-form">-->
+			<!--				<h1 class="search-form__title title">Найти билеты</h1>-->
+			<!--				<div class="search-form__nav">-->
+			<!--					<div class="form-nav">-->
+			<!--						<a class="form-nav__item form-nav__item_active" href="/"><span class="form-nav__icon"><img-->
+			<!--									src="/f/svg/icn-search.svg"/></span><span class="form-nav__name">Найти билеты</span></a>-->
+			<!--						<a class="form-nav__item form-nav__item_disable" href="/seats.html"><span-->
+			<!--								class="form-nav__icon"><img src="/f/svg/icn-seat.svg"/></span><span-->
+			<!--								class="form-nav__name">Выбрать места</span></a>-->
+			<!--						<a class="form-nav__item form-nav__item_disable" href="/profile.html"><span-->
+			<!--								class="form-nav__icon"><img src="/f/svg/icn-ticket.svg"/></span><span-->
+			<!--								class="form-nav__name">Данные билетов</span></a>-->
+			<!--						<a class="form-nav__item form-nav__item_disable" href="/check.html"><span-->
+			<!--								class="form-nav__icon"><img src="/f/svg/icn-check.svg"/></span><span-->
+			<!--								class="form-nav__name">Проверка</span></a>-->
+			<!--					</div>-->
+			<!--				</div>-->
+			<!--				<div class="search-form__container">-->
+			<!--					<div class="direction">-->
+			<!--						<div class="direction__item">-->
+			<!--							<select>-->
+			<!--								<option value="Элиста — Москва">Элиста — Москва</option>-->
+			<!--								<option value="Москва — Элиста">Москва — Элиста</option>-->
+			<!--							</select>-->
+			<!--						</div>-->
+			<!--						<div class="direction__item">-->
+			<!--							<input type="date">-->
+			<!--						</div>-->
+			<!--						<div class="direction__item">-->
+			<!--							<button class="btn" type="submit">Найти</button>-->
+			<!--						</div>-->
+			<!--					</div>-->
+			<!--					<table class="list-direction">-->
+			<!--						<thead>-->
+			<!--						<tr>-->
+			<!--							<th></th>-->
+			<!--							<th>Направление</th>-->
+			<!--							<th>Отправление</th>-->
+			<!--							<th>Прибытие</th>-->
+			<!--							<th>Стоимость</th>-->
+			<!--							<th>Мест</th>-->
+			<!--						</tr>-->
+			<!--						</thead>-->
+			<!--						<tbody class="table-direction">-->
+			<!--						<tr>-->
+			<!--							<td>-->
+			<!--								<input type="radio">-->
+			<!--							</td>-->
+			<!--							<td>Элиста - Москва</td>-->
+			<!--							<td class="table-direction__date">-->
+			<!--								09.07.2016-->
+			<!--								<span>12:07</span>-->
+			<!--							</td>-->
+			<!--							<td class="table-direction__date">-->
+			<!--								10.07.2016-->
+			<!--								<span>04:07</span>-->
+			<!--							</td>-->
+			<!--							<td class="table-direction__cost">2200 ₽</td>-->
+			<!--							<td>15</td>-->
+			<!--						</tr>-->
+			<!--						</tbody>-->
+			<!--					</table>-->
+			<!--				</div>-->
+			<!--				<div class="search-form__footer">-->
+			<!--					<a class="search-form__next-step btn" href="/seats.html">Выбрать</a>-->
+			<!--				</div>-->
+			<!--			</div>-->
+		</div>
+	</div>
+	<div class="footer">
+		<div class="footer__wrapper">
+			<div class="footer__copyright">© 2016, OOO «Спринт-Тур». Все права защищены. «Спринт-Тур» — Онлайн сервис
+				продажи билетов
 			</div>
-
-			<div class="advantage">
-				<div class="advantage__item">
-					<div class="advantage__image"></div>
-					<p class="advantage__title">wi-fi</p>
+			<div class="footer__contacts">
+				<div class="footer__item">
+					<a class="footer__tel" href="tel:+7 905 400-04-68">+7 905 400-04-68</a>
 				</div>
-				<div class="advantage__item">
-					<div class="advantage__image"></div>
-					<p class="advantage__title">подушки и пледы</p>
-				</div>
-				<div class="advantage__item">
-					<div class="advantage__image"></div>
-					<p class="advantage__title">Телевизор</p>
-				</div>
-				<div class="advantage__item">
-					<div class="advantage__image"></div>
-					<p class="advantage__title">Вода</p>
-				</div>
-				<div class="advantage__item">
-					<div class="advantage__image"></div>
-					<p class="advantage__title">Бесплатное такси <br>в пределах МКАД</p>
-				</div>
-			</div>
-
-			<div class="content__text">
-				<div class="content__desc">
-					<p class="text">
-						Альбатрос прекрасно применяет вечнозеленый кустарник, а из холодных закусок можно выбрать
-						плоские колбасы "луканка" и "суджук". Флора и фауна перевозит вулканизм.
-					</p>
-				</div>
-			</div>
-
-			<h2 class="title title_ta-c title_fz-l" id="actions">Акции</h2>
-
-			<div class="content__text">
-				<div class="content__desc">
-					<p class="text">
-						Из первых блюд распространены супы-пюре и бульоны, но подают их редко, тем не менее производство
-						зерна и зернобобовых недоступно превышает символический центр современного Лондона, ни для кого
-						не секрет, что Болгария славится масличными розами, которые цветут по всей Казанлыкской долине.
-					</p>
-					<a href="#" class="btn btn_bg-pink">Подробнее</a>
-				</div>
-				<div class="content__image">
-					<img src="<?php echo Yii::app()->theme->getBaseUrl(); ?>/img/img_bus-1.jpg" alt="Автобус">
-				</div>
-			</div>
-			<h2 class="title title_ta-c title_fz-l" id="contacts">Контакты</h2>
-
-			<div class="content__text">
-				<div class="content__image">
-					<script type="text/javascript" charset="utf-8" async
-							src="https://api-maps.yandex.ru/services/constructor/1.0/js/?sid=cv_Cgnxo2kS8QKBMoGerEbcBT0OHIX5O&width=448&height=277&lang=ru_RU&sourceType=constructor&scroll=true"></script>
-				</div>
-				<div class="content__desc">
-					<p class="text">
-						БРОНИРОВАНИЕ ПО НОМЕРУ +7-905-400-0468
-					</p>
+				<div class="footer__item">
+					<a class="footer__email" href="mail:info@sprint-tour-08.ru">info@sprint-tour-08.ru</a>
 				</div>
 			</div>
 		</div>
 	</div>
-<?php } ?>
+</div>
+<div class="popup"></div>
+<link href="./f/css/style.css" rel="stylesheet">
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+<script src="/f/js/script.js"></script>
 </body>
 </html>
